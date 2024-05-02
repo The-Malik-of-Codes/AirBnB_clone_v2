5a15ec127fd428e881cc936a65e657d95ce5981e
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2024-05-02 09:47:30 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,6 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-<<<<<<< HEAD
         """returns a dictionary
         Return:
             returns a dictionary of __object
@@ -38,18 +37,6 @@ class FileStorage:
             return (dic)
         else:
             return self.__objects
-=======
-        """Returns a dictionary of models currently in storage"""
-        dictionary = {}
-        dict = self.__objects
-        if cls is not None:
-            for ky in dict:
-                val = ky.replace('.', ' ')
-                val = shlex.split(val)
-                if (val[0] == cls.__name__):
-                    dictionary[ky] = self.__objects[ky]
-        return dictionary
->>>>>>> refs/remotes/origin/master
 
     def new(self, obj):
         """sets __object to given obj
@@ -73,7 +60,6 @@ class FileStorage:
         """serialize the file path to JSON file path
         """
         try:
-<<<<<<< HEAD
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
                 for key, value in (json.load(f)).items():
                     value = eval(value["__class__"])(**value)
@@ -92,22 +78,3 @@ class FileStorage:
         """ calls reload()
         """
         self.reload()
-=======
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-
-        def delete(self, obj=None):
-            """Delete obj from __objects if it’s inside - if obj is == None"""
-            if obj is not None:
-                ky = "{}.{}".format(type(obj).__name__, obj.id)
-                if ky in FileStorage.__objects:
-                    print("Deleting key:", ky)
-                    del FileStorage.__objects[ky]
-                else:
-                    print("Key not found:", ky)
->>>>>>> refs/remotes/origin/master
